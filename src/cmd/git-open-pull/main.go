3e48cda58ab66ffb0255110eb27f83d7b45c314d
Jehiah Czebotar <jehiah@gmail.com>
2017-06-15 15:01:13 -0400

    list issues from github

--- src/cmd/git-open-pull/main.go
+++ src/cmd/git-open-pull/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
@@ -22,6 +25,24 @@ func main() {
 	issueNumber := DetectIssueNumber(branch)
 	if issueNumber != 0 {
 		fmt.Printf("issue number %d\n", issueNumber)
+	}
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: settings.Token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	client.UserAgent = "git-open-pull/1.0 (+http://github.com/jehiah/git-open-pull)"
+
+	// get labels
+	labels, _, err := client.Issues.ListLabels(ctx, settings.BaseAccount, settings.BaseRepo, &github.ListOptions{PerPage: 100})
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+	for _, l := range labels {
+		if l.Name != nil {
+			fmt.Printf("label:%s\n", *l.Name)
+		}
 	}
 
 }
--- vendor.sh
+++ vendor.sh
@@ -7,3 +7,6 @@ fi
 
 
 gb vendor fetch -no-recurse -revision 7a51fb928f52a196d5f31daefb8a489453ef54ff github.com/google/go-github
+gb vendor fetch -no-recurse -revision f047394b6d14284165300fd82dad67edb3a4d7f6 golang.org/x/oauth2
+gb vendor fetch -no-recurse -revision 53e6ce116135b80d037921a7fdd5138cf32d7a8a github.com/google/go-querystring
+gb vendor fetch -no-recurse -revision ddf80d0970594e2e4cccf5a98861cad3d9eaa4cd golang.org/x/net/context
